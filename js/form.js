e42b943848156a40cf7a4562b6443f8763be0438
ZARZAF <73442523+ZARZAF@users.noreply.github.com>
2024-01-14 14:21:24 +0200

    Ver 2

--- js/form.js
+++ js/form.js
@@ -1,20 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const formButton = document.getElementById('form-button');
-    const formCloseButton = document.getElementById('btn-cancel');
+	const formRegistrationButton = document.getElementById('btn-register');
+    const formCloseButtonSignIn = document.getElementById('btn-cancel-signIn');
+	const formCloseButtonReg = document.getElementById('btn-cancel-reg');
 	
     function openSignInForm() {
+		document.getElementById("title-video").style.display = "none";
+		document.getElementById("title-image").style.display = "block";
 		document.getElementById("signInForm").style.display = "block";
+	}
+	
+	function openRegistrationForm() {
+		document.getElementById("signInForm").style.display = "none";
+		document.getElementById("registrationForm").reset();
+		document.getElementById("registrationForm").style.display = "block";
 	}
 	
 	function closeSignInForm() {
 		document.getElementById("signInForm").style.display = "none";
 	}
 	
+	function closeRegistrationForm() {
+		document.getElementById("registrationForm").style.display = "none";
+	}
+	
     if (formButton) {
         formButton.addEventListener('click', openSignInForm);
     }
 	
-	if (formCloseButton) {
-        formCloseButton.addEventListener('click', closeSignInForm);
+	if (formRegistrationButton) {
+        formRegistrationButton.addEventListener('click', openRegistrationForm);
+    }
+	
+	if (formCloseButtonSignIn) {
+        formCloseButtonSignIn.addEventListener('click', closeSignInForm);
+    }
+	
+	if (formCloseButtonReg) {
+        formCloseButtonReg.addEventListener('click', closeRegistrationForm);
     }
 });
