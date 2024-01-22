bfcb377b88ae9e3362783a676debb7da78471adb
ZARZAF <73442523+ZARZAF@users.noreply.github.com>
2024-01-22 18:39:46 +0200

    php

---
+++ api.php
@@ -0,0 +1,157 @@
+<?php
+require "db.php";
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header('Content-Type: application/json');
+
+$response = null;
+// Check if it's a POST request
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve the POST data
+	$action = isset($_POST['action']) ? $_POST['action'] : null;
+	switch ($action) {
+		case 'login':
+            $username = isset($_POST['username']) ? $_POST['username'] : null;
+			$password = isset($_POST['password']) ? $_POST['password'] : null;
+
+			if ($username !== null && $password !== null) {
+				$response = login($username, $password);
+			} else {
+				// If either username or password is missing
+				$response = array('error' => 'Both username and password are required.');
+			}
+            break;
+
+        case 'register':
+            $username = isset($_POST['username']) ? $_POST['username'] : null;
+            $fn = isset($_POST['fn']) ? $_POST['fn'] : null;
+			$password = isset($_POST['password']) ? $_POST['password'] : null;
+            $response = register($username, $fn, $password);
+            break;
+
+		case 'get_admin_status':
+			$username = isset($_POST['username']) ? $_POST['username'] : null;
+			$adminStatus = getAdminStatus($username);
+			if($adminStatus === null) {
+				return array('error' => 'Admin status is null');
+			}
+			return array('message' => 'Login successful for ' . $username, 'adminStatus' => $adminStatus);
+			
+		case 'presentations':
+			$postData = isset($_POST['data']) ? $_POST['data'] : null;
+			$decoded_json = json_decode($postData, true);
+
+			foreach ($decoded_json as $key => $value) {
+				foreach($value as $row) {
+					$msg = addPresentationToDB($row[0], $key, $row[1], $row[2] , $row[3], $row[4], $row[5], $row[6]);
+				}
+			}
+			$response = array('message' => 'success');
+			
+		case 'load_presentations':
+			$result = getAllPresentations();
+			if ($result == null) {
+				$response = array('error' => 'Data base is empty');
+				break;
+			}
+			$result = fixAllDates($result);
+			$result = fixEmptyInts($result);
+			$dict = turnPresentationsIntoDict($result);
+			$response = json_encode($dict);
+			break;
+			
+		case 'drop_presentations':
+			dropAllPresentations();
+			$response = array('message' => 'success');
+			break;
+		
+		case 'drop_interests':
+			dropAllInterests();
+			$response = array('message' => 'success');
+			break;
+			
+		case 'get_presentation_id':
+			$fn = isset($_POST['fn']) ? $_POST['fn'] : null;
+			$result = getPresentationId($fn);
+			if ($result == null) {
+				$response = array('error' => 'Fn not found, no Id to return');
+			}
+			$response = $result;
+			break;
+		
+		case 'get_user_id':
+			$username = isset($_POST['username']) ? $_POST['username'] : null;
+			$result = getUserIdByUsername($username);
+			if ($result == null) {
+				$response = array('error' => 'Username not found, no Id to return');
+			}
+			$response = $result;
+			break;
+			
+		case 'add_interest':
+			$userId = isset($_POST['userId']) ? $_POST['userId'] : null;
+            $presentationId = isset($_POST['presentationId']) ? $_POST['presentationId'] : null;
+			$interestString = isset($_POST['interestString']) ? $_POST['interestString'] : null;
+            $response = addInterestToDb($userId, $presentationId, $interestString);
+			break;
+			
+		/*case 'get_all_interests':
+			$result = getAllInterests();
+			if ($result == null) {
+				$response = array('error' => 'Data base is empty, no interests found');
+				break;
+			}
+			$response = json_encode($result);
+			break;
+			*/
+		case 'get_interests':
+			$userId = isset($_POST['userId']) ? $_POST['userId'] : null;
+			$result = getUserInterests($userId);
+			if ($result == null) {
+				$response = array('error' => 'Data base is empty, no interests found');
+				break;
+			}
+			$response = json_encode($result);
+			break;
+		
+		/*case 'get_presentation_by_id':
+			$presentationId = isset($_POST['presentationId']) ? $_POST['presentationId'] : null;
+			$result = getPresentationById($presentationId);
+			if ($result == null) {
+				$response = array('error' => 'Id not found, no presentation to return');
+			}
+			$response = $result;
+			break; */
+        default:
+            $response = array('error' => 'Invalid action. Fail at js fetch to api.php');
+            break;
+	}
+    
+} 
+// Output the response as JSON
+echo json_encode($response);
+
+function login($username, $password) {
+    if(!doesUserExist($username)) {
+		return array('error' => 'Username or password is incorrect.');
+	}
+	if (!correctUsernameAndPassword($username, $password)) {
+		return array('error' => 'Username or password is incorrect.');
+	}
+	$adminStatus = getAdminStatus($username);
+	if($adminStatus === null) {
+		return array('error' => 'Admin status is null');
+	}
+    return array('message' => 'Login successful for ' . $username, 'adminStatus' => $adminStatus);
+}
+
+function register($username, $fn, $password) {
+	if(doesUserExist($username)) {
+		return array('error' => 'Username is already used.');
+	}
+	$output = addUserToDB($username, $fn, $password);
+	return array('message' => 'Registration successful for ' . $username, 'DBresponse' => $output);
+}
+
+?>
---
+++ db.php
@@ -0,0 +1,281 @@
+<?php
+/*$conn = new PDO('mysql:host=localhost;dbname=webproject', 'root', '');
+$sql = "SELECT * FROM users";
+$query = $conn->query($sql) or die("failed!");
+while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+ echo $row['username'] . "\n";
+}*/
+?>
+<?php
+
+class Db
+{
+    private $connection;
+    private $tableName;
+    private $dbName;
+
+    public function __construct($tableName)
+    {
+        $dbhost = "localhost";
+        $userName = "root";
+        $userPassword = "";
+        $this->dbName = "webproject";
+
+        $this->connection = new PDO("mysql:host=$dbhost;dbname=$this->dbName", $userName, $userPassword);
+          /*  [
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]);
+			*/
+        $this->tableName = $tableName;
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+	public function escapeString($input) {
+		return addslashes($input);
+	}
+	public function query($sql) {
+		$stmt = $this->connection->prepare($sql);
+		$stmt->execute();
+		return $stmt;
+	}
+}
+
+function convertDateFormatToSQL($inputDate) {
+	 $date = DateTime::createFromFormat('d.m.Y', $inputDate);
+    
+    if ($date) {
+        // Convert the date to the desired format
+        $outputDate = $date->format('Y-m-d');
+        return $outputDate;
+    } else {
+        // Invalid input date format
+        return "Invalid date format. Please use DD.MM.YYYY.";
+    }
+}
+
+function convertSQLDateFormatToJS($inputDate) {
+	 $date = DateTime::createFromFormat('Y-m-d', $inputDate);
+     
+    if ($date) {
+        // Convert the date to the desired format
+        $outputDate = $date->format('d.m.Y');
+        return $outputDate;
+    } else {
+        // Invalid input date format
+        return "Invalid date format";
+    }
+}
+
+
+function getUserIdByUsername($username) {
+	$db = new Db('users'); 
+	$username = $db->escapeString($username);
+	$sql = "SELECT userId FROM users WHERE username = '$username'";
+    $result = $db->query($sql);
+	if ($result->rowCount() > 0) {
+        $id = $result->fetch(PDO::FETCH_ASSOC);
+        return $id;
+    } else {
+        return null;
+    }
+}
+
+function doesUserExist($username) {
+	$db = new Db('users'); 
+	$username = $db->escapeString($username);
+	$sql = "SELECT * FROM users WHERE username = '$username'";
+    $result = $db->query($sql);
+	if ($result->rowCount() > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function correctUsernameAndPassword($username, $password) {
+	$db = new Db('users'); 
+	$username = $db->escapeString($username);
+	$password = $db->escapeString($password);
+	$sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $result = $db->query($sql);
+	if ($result->rowCount() > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function getAdminStatus($username) {
+	$db = new Db('users'); 
+	$username = $db->escapeString($username);
+	$sql = "SELECT admin FROM users WHERE username = '$username'";
+    $result = $db->query($sql);
+	if ($result->rowCount() > 0) {
+        $adminStatus = $result->fetch(PDO::FETCH_ASSOC);
+        return $adminStatus;
+    } else {
+        return null;
+    }
+}
+
+function addUserToDB($username, $fn, $password){
+	$db = new Db('users'); 
+	$username = $db->escapeString($username);
+	$fn = $db->escapeString($fn);
+	$password = $db->escapeString($password);
+	$sql = "INSERT INTO `users` (`userId`, `fn`, `username`, `password`, `admin`) VALUES (NULL, '$fn', '$username', '$password', '0')";
+	$result = $db->query($sql);
+	return  $result->fetch(PDO::FETCH_ASSOC); // maybe error causing
+}
+
+function addPresentationToDB($orderId, $date, $hour, $fn , $groupStudent, $name, $topicId, $topic){
+	$db = new Db('presentations'); 
+	$date = convertDateFormatToSQL($date);
+	$fn = $db->escapeString($fn);
+	$name = $db->escapeString($name);
+	$topic = $db->escapeString($topic);
+	$sql = "INSERT INTO `presentations` (`id`, `orderId`, `date`, `hour`, `fn`, `groupStudent`, `name`, `topicId`, `topic`) VALUES (NULL, '$orderId', '$date', '$hour', '$fn' , '$groupStudent' , '$name' , '$topicId', '$topic')";
+	$result = $db->query($sql);
+	return  $result->fetch(PDO::FETCH_ASSOC); // maybe error causing
+}
+
+function getAllPresentations(){
+	$db = new Db('presentations'); 
+	$sql = "SELECT `orderId`, `date`, `hour`, `fn`, `groupStudent`, `name`, `topicId`, `topic`  FROM `presentations`";
+    $result = $db->query($sql);
+	if ($result->rowCount() > 0) {
+        $presentationsRows = $result->fetchAll(PDO::FETCH_ASSOC);
+        return $presentationsRows;
+    } else {
+        return null;
+    }
+}
+
+function fixAllDates($presentations) {
+	foreach ($presentations as &$value) {
+		if (isset($value["date"])){
+			$value["date"] = convertSQLDateFormatToJS($value["date"]);	
+		}
+	}
+	return $presentations;
+}
+
+function fixEmptyInts($presentations) {
+	foreach ($presentations as &$value) {
+		if ($value["fn"] == '') {
+			$value["groupStudent"] = '';
+		}
+		if ($value["topicId"] == 0) {
+			$value["topicId"] = '';
+		}
+	}
+	return $presentations;
+}
+
+function extractEverythingButDate($arrayPresnetation) {
+	$res = [];
+	foreach($arrayPresnetation as $key => $value){
+		if($key != "date") {
+			array_push($res, $value);
+		}
+	}
+	return $res;
+}
+
+function turnPresentationsIntoDict($presentations) {
+	$dictionary = [];
+
+	foreach ($presentations as $subArray) {
+		if (isset($subArray["date"])) {
+			$keyDict = $subArray["date"];
+			if(!array_key_exists($keyDict, $dictionary)){
+				$dictionary[$keyDict] = [];
+			}
+			array_push($dictionary[$keyDict], extractEverythingButDate($subArray));
+		}
+	}
+	return $dictionary;
+}
+
+function dropAllPresentations() {
+	$db = new Db('presentations'); 
+	$sql = "TRUNCATE TABLE presentations";
+    $result = $db->query($sql);
+	return  $result->fetch(PDO::FETCH_ASSOC);
+}
+
+function dropAllInterests() {
+	$db = new Db('interests'); 
+	$sql = "TRUNCATE TABLE interests";
+    $result = $db->query($sql);
+	return  $result->fetch(PDO::FETCH_ASSOC);
+}
+
+function getPresentationId($fn) {
+	$db = new Db('presentations'); 
+	$fn = $db->escapeString($fn);
+	$sql = "SELECT id FROM presentations WHERE fn = '$fn'";
+    $result = $db->query($sql);
+	if ($result->rowCount() > 0) {
+        $id = $result->fetch(PDO::FETCH_ASSOC);
+        return $id;
+    } else {
+        return null;
+    }
+}
+
+function addInterestToDb($userId, $presentationId, $interestString) {
+	$db = new Db('interests'); 
+	$interestString = $db->escapeString($interestString);
+	//$sql = "INSERT INTO `interests` (`id`, `userId`, `presentationId`, `interestType`) VALUES (NULL, '$userId', '$presentationId', '$interestString')";
+	$sql = "UPDATE `interests` SET `interestType` = '$interestString' WHERE `userId` = '$userId' AND `presentationId` = '$presentationId'";
+	$sql2 = "INSERT INTO `interests` (`id`, `userId`, `presentationId`, `interestType`) SELECT NULL, '$userId', '$presentationId', '$interestString' WHERE ROW_COUNT() = 0";
+	$result = $db->query($sql);
+	$result->fetch(PDO::FETCH_ASSOC);
+	$result2 = $db->query($sql2);
+	$result2->fetch(PDO::FETCH_ASSOC);
+	return  $result2;
+}
+
+function getAllInterests(){
+	$db = new Db('interests'); 
+	$sql = "SELECT userId, presentationId, interestType FROM interests";
+    $result = $db->query($sql);
+	if ($result->rowCount() > 0) {	
+        $interestsRow = $result->fetchAll(PDO::FETCH_ASSOC);
+        return $interestsRow;
+    } else {
+        return null;
+    }
+}
+
+function getUserInterests($userId) {
+	$db = new Db('interests'); 
+	$sql = "SELECT interestType, orderId, date, hour, fn, groupStudent, name, topicId, topic FROM interests JOIN presentations ON presentations.id = presentationId WHERE interests.userId = '$userId'" ;
+    $result = $db->query($sql);
+	if ($result->rowCount() > 0) {	
+        $interestsRow = $result->fetchAll(PDO::FETCH_ASSOC);
+        return $interestsRow;
+    } else {
+        return null;
+    }
+}
+
+function getPresentationById($presentationId) {
+	$db = new Db('presentations'); 
+	$presentationId = $db->escapeString($presentationId);
+	$sql = "SELECT * FROM presentations WHERE id = '$presentationId'";
+    $result = $db->query($sql);
+	if ($result->rowCount() > 0) {
+        $presentation = $result->fetch(PDO::FETCH_ASSOC);
+        return $presentation;
+    } else {
+        return null;
+    }
+}
+
+?>
